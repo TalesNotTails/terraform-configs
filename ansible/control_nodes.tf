cf4b5c4fd7e62a269a6f5182502850b9ea5a3f83
TalesNotTails <c.jetta2014@gmail.com>
2025-07-17 23:44:05 -0400

    added s3 backend and amis for ansible nodes

--- ansible/control_nodes.tf
+++ ansible/control_nodes.tf
@@ -1,13 +1,5 @@
-resource "aws_instance" "example_server" {
-  ami           = "ami-00963c6c64afa4994"
+resource "aws_instance" "ansible_ctrl_node" {
+  ami           = "ami-0441a74d496e1ca07"
   instance_type = "t2.micro"
-  key_name = "Macbook Key"
-  user_data = <<EOF
-#!/bin/bash
-rhc connect --activation-key activation-key-default-su4zls --organization 19207058
-EOF
   security_groups = [ "EC2-Default-SG" ]
-  tags = {
-    name = "testVM"
-  }
 }
--- terraform.tf
+++ terraform.tf
@@ -5,4 +5,11 @@ terraform {
       version = "6.2.0"
     }
   } 
+  backend "s3" {
+    bucket = "tf-rb-cs"
+    key = "state/terraform.state"
+    region = "us-east-1"
+    encrypt = true
+    use_lockfile = true
+  }
 } 
