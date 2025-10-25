e82a2375a22fa0d68593d3ea746c71644f34e304
TalesNotTails <c.jetta2014@gmail.com>
2025-10-25 10:36:52 -0400

    removed key pairs module

--- key_pairs/macbook_key_pair.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_key_pair" "mac_key" {
-  key_name   = "Macbook Key"
-  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB4mTLwN2A7Cnj80ftMrriRZ+lXIU7cCZKcNOH7lo7BD csariaslani@Cyruss-MacBook-Pro.local"
-}
--- main.tf
+++ main.tf
@@ -1,11 +1,18 @@
+resource "aws_instance" "ansible_dev_node" {
+  ami           = "ami-0853d3a745e6fa2c3"
+  instance_type = "t2.micro"
+  security_groups = [ "EC2-Default-SG" ]
+  user_data = <<EOF
+  #!/bin/bash
+  cd /home/ansible/
+  git clone git@github.com:TalesNotTails/Ansible.git
+  EOF
+  tags = {
+    Type = "ansible_dev_node"
+  }
+}
+
 module "security_groups" {
   source = "./sec_groups"
 }
 
-module "key_pairs" {
-  source = "./key_pairs"
-}
-
-module "ansible_ctrl_node" {
-  source = "./ansible"
-}
