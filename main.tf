6b34738e8e479be5ff5054d92480219f1b7ddf7c
TalesNotTails <c.jetta2014@gmail.com>
2025-10-27 00:39:57 -0400

    added attatchment resource

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "ansible_dev_node" {
   ami           = "ami-0e9e3701ddd593b08"
   instance_type = "t2.small"
   security_groups = [ "EC2-Default-SG" ]
+  availability_zone = module.ebs_volumes.bois_az
   root_block_device {
     delete_on_termination = true
   }
@@ -13,6 +14,17 @@ resource "aws_instance" "ansible_dev_node" {
   tags = {
     Type = "ansible_dev_node"
   }
+}
+
+resource "aws_volume_attachment" "ebs_att" {
+  device_name = "/dev/sdb"
+  volume_id = module.ebs_volumes.bois_ebs_id
+  instance_id = aws_instance.ansible_dev_node.id
+  skip_destroy = true
+}
+
+module "ebs_volumes" {
+  source = "./ebs_vols"
 }
 
 module "security_groups" {
